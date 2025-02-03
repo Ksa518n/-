8c166b18d883e38ca27ddf604a33aa5b1c765eb6
Ksa518n <159606491+Ksa518n@users.noreply.github.com>
2025-02-03 20:10:01 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-
+// يمكنك إضافة أي تفاعلية تريدها هنا
+document.querySelectorAll('.social-link').forEach(link => {
+    link.addEventListener('click', () => {
+        alert(`سيتم توجيهك إلى ${link.textContent}`);
+    });
+});
